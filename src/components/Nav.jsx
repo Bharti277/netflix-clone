ed8ef2061cfc157c67066fcc6c4deb4bb46679a4
Bharti277 <bhartideependra277@gmail.com>
2024-02-08 15:16:34 +0530

    navbar wip

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Netflix from "../assets/netflix.jpg";
 import "./Nav.css";
 
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    // return () => {
+    //   window.removeEventListener("scroll");
+    // };
+  }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img src={Netflix} alt="Netflix" />
     </div>
   );
